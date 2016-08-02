739a9c8204361dab7b89217d6e2a5114656d05f8
marthaberner <marthaberner.mb@gmail.com>
2016-08-02 12:30:58 -0600

    user can upvote and downvote

--- js/controllers/posts_controller.js
+++ js/controllers/posts_controller.js
@@ -1,3 +1,12 @@
 app.controller('PostsController', ['$scope', 'PostsFactory', function ($scope, PostsFactory) {
   $scope.posts = PostsFactory;
+  $scope.upvote = function (post) {
+    post.votes += 1
+  }
+  $scope.downvote = function (post) {
+    post.votes -= 1
+  }
+  $scope.getClass = function (post) {
+    return post.votes > 0 ? 'green' : post.votes < 0 ? 'red' : ''
+  }
 }])
